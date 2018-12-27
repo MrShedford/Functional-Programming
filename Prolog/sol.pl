2a2ea73745fb7903069e5e9250f75911b199f86d
Sean Edward <seanedward@Seans-MacBook-Pro.local>
2018-12-27 14:04:30 +0000

    now contains exercise 2

--- Prolog/sol.pl
+++ Prolog/sol.pl
@@ -1,16 +1,39 @@
+/* Exercise 1 */
+
 xCount([H1 | T], C) :- =(H1, x), !, NewC is C + 1, xCount(T, NewC); xCount(T, C).
 xCount([], A) :- A is 1.
 
 countOfCounts([], S) :- S is 1.
 countOfCounts([H | T], S) :- xCount(H, 0), !, X is S + 1, countOfCounts(T, X); countOfCounts(T, S).
 solved(A) :- countOfCounts(A, 0).
- /*
- Solved takes in a List of Lists (A) and passes them to count of counts, and passes in 0, being the starting number for the amount of times we've seen the x peg so far
-countOfCounts takes the list of lists and splits it with H being the first thing in the list, T being everything that follows (similiar to Scheme with car and cdr) and S being the count
-It runs xCount on the H (being one row of this board) 
-If xCount returns true (ie there's an x on that row) then we run everything to the right of the exclamation point until the semi colon (we increase the overall count by one and pass the rest pf the list in recursively)
-If it returns false then we pass the rest of the list in with our original value of S, since it didn't increase by one
-Our basecase is if we have an empty list then S is 1
 
-xCount takers in a list and checks each character for x and increases the counter each time it sees an x 
- */
+/*Exercise 2 */
+%% horizontal
+checkHorizontalJump([H1 | T1], [H2 | T2], DifferenceCount) :- \+ =(H1, H2), !, NewCount is DifferenceCount + 1, checkHorizontalJump(T1, T2, NewCount); checkHorizontalJump(T2, T2, DifferenceCount).
+checkHorizontalJump([], [], DifferenceCount) :- DifferenceCount is 3; DifferenceCount is 0.
+
+isCorrectHorizontalJump([H1 | T1], [H2 | T2]) :- checkHorizontalJump(H1, H2, 0), !, isCorrectHorizontalJump(T1, T2).
+isCorrectHorizontalJump([], []).
+                           
+containsHorizontalJump([], _, OCount, FoundConsecutiveX) :- OCount > 0, FoundConsecutiveX is 1.
+containsHorizontalJump([H | T], IsLastX, OCount, FoundConsecutiveX) :- =(H, x), !, (=(IsLastX, 1), !, containsHorizontalJump(T, 1, OCount, 1); containsHorizontalJump(T, 1, OCount, FoundConsecutiveX)); NewOCount is OCount + 1, containsHorizontalJump(T, 0, NewOCount, FoundConsecutiveX). 
+
+isHorizontalJump([], XCount) :- XCount is 1. % must have one instance of two x's
+isHorizontalJump([H | T], XCount) :- containsHorizontalJump(H, 0, 0, 0), !, NewXCount is XCount + 1, isHorizontalJump(T, NewXCount); isHorizontalJump(T, XCount).
+
+
+%% vertical
+checkVerticalJump([H1 | T1], [H2 | T2], DifferenceCount) :- \+ =(H1, H2), !, NewCount is DifferenceCount + 1, checkVerticalJump(T1, T2, NewCount); checkVerticalJump(T1, T2, DifferenceCount).
+checkVerticalJump([], [], DifferenceCount) :- DifferenceCount is 1.
+
+isCorrectVerticalJump([H1 | T1], [H2 | T2]) :- checkVerticalJump(H1, H2, 0), !, isCorrectVerticalJump(T1, T2).
+isCorrectVerticalJump([], []).
+
+containsVerticalJump([], XCount) :- XCount is 1.
+containsVerticalJump([H | T], XCount) :- =(H, x), !, NewCount is XCount + 1, containsVerticalJump(T, NewCount); containsVerticalJump(T, XCount).
+
+isVerticalJump([], XCount) :- XCount is 2. % must have one instance of two x's
+isVerticalJump([H | T], XCount) :- containsVerticalJump(H, 0), !, NewXCount is XCount + 1, isVerticalJump(T, NewXCount); isVerticalJump(T, XCount).
+
+
+jump(BoardOne, BoardTwo) :- isHorizontalJump(BoardOne, 0), !, isCorrectHorizontalJump(BoardOne, BoardTwo); isVerticalJump(BoardOne, 0), !, isCorrectVerticalJump(BoardOne, BoardTwo).
